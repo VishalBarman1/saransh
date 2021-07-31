ea97b1ea605ac4bcf95db1b932f5e2fff625aa46
VishalBarman1 <81417884+VishalBarman1@users.noreply.github.com>
2021-07-31 08:07:27 +0530

    s

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-	[general]\n\
-	email=\"barman.vishal.2020@gmail.com\"\n\
-	" > ~/.streamlit/credentials.toml
-
-echo "\
 	[server]\n\
 	headless = true\n\
 	port = $PORT\n\
