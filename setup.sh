e0a8f460afd4cbef7b4ff679b1acf065ff23ee33
VishalBarman1 <81417884+VishalBarman1@users.noreply.github.com>
2021-07-31 08:23:25 +0530

    d

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-	[server]\n\
-	headless = true\n\
-	port = $PORT\n\
-	enableCORS = false\n\
-	\n\
-	" > ~/.streamlit/config.toml
+[general]\n\
+email = \"barman.vishal.2020@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
